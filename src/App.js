95176592020b646f79c65f5051cac7f0a663a7d9
trushti5 <trushtikotadiya.dreamspot@gmail.com>
2024-03-06 15:05:22 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,183 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='sd'>
+      <div style={{ paddingBottom: '5.20%' }}>
+        <div >
+          <div
+            style={{ display: 'flex', justifyContent: 'space-between', backgroundColor: 'rgba(110, 111, 109, 0.476)', paddingTop: '-10%' }} className='stick'>
+            <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ081YJe13zskB0MXEG71sv6VJnDoCNRIiN1KESHsAAsETLuFi6"
+              width="150px" height="100px" style={{ background: 'none' }} />
+            <div className="btn-group,container " >
+              <div style={{ display: 'flex', gap: '0px', marginTop: '10px' }}><button type="button" className="btn p-3 s"
+                style={{ borderRight: '1px solid rgb(91, 88, 88)', height: '50px' }}>
+                <img src="https://cdn-icons-png.flaticon.com/128/483/483141.png" width="30px" height="30px" />
+              </button>
+
+                <button type="button" className="btn p-3 s"
+                  style={{ borderLeftt: ' 1px solid rgb(135, 130, 130)', height: '50px' }}><img
+                    src="https://cdn-icons-png.flaticon.com/128/0/191.png" width="30px" height="30px" />
+                </button>
+
+              </div>
+
+            </div>
+            <div style={{ display: ' flex', alignItems: ' center', gap: '30px' }} className="flex-wrap">
+              <div>
+                <p>Click edit and create your own amazing website
+
+                </p>
+              </div>
+              <div style={{ marginTop: '-20px' }} className=" flex-wrap"> <a
+                href="https://users.wix.com/signin?originUrl=https:%2F%2Fmanage.wix.com%2Fedit-template%2Ffrom-intro%3ForiginTemplateId%3De5270f42-25b8-4f7a-aafb-2e609a06e17f%26editorSessionId%3D820501da-9c2d-4420-96a9-a7c1232b9e30&redirectTo=https:%2F%2Fmanage.wix.com%2Fedit-template%2Ffrom-intro%3ForiginTemplateId%3De5270f42-25b8-4f7a-aafb-2e609a06e17f%26editorSessionId%3D820501da-9c2d-4420-96a9-a7c1232b9e30&overrideLocale=en&forceRender=true">Read
+                More</a>
+              </div>
+
+
+              <button type="button" className="btn btn-primary c" style={{ marginTop: '-10px', }}>Edit this site</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style={{ backgroundImage: 'url(https://static.wixstatic.com/media/84770f_55e52c1ac81f450fadd6509cbc317d0b~mv2.jpg/v1/fill/w_1905,h_951,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/84770f_55e52c1ac81f450fadd6509cbc317d0b~mv2.jpg)', backgroundSize: 'cover', height: '900px' }}>
+        <div style={{ display: 'flex', justifyContent: "space-around" }} className='uy' >
+          <div className='az'> <h1 >CKP</h1></div>
+          <div className='ac'>
+            <a href='#f' style={{ color: 'white' }}>Portfolio</a>
+            <a href='#ac' style={{ color: 'white' }}> About</a>
+            <a href='#aw' style={{ color: 'white' }}> Albums</a>
+            <a href='#we' style={{ color: 'white' }}>Contact</a>
+            <a style={{ marginTop: '-5px' }}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-facebook" viewBox="0 0 16 16">
+              <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+            </svg></a>
+            <a style={{ marginTop: '-5px' }}><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-instagram" viewBox="0 0 16 16">
+              <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+            </svg></a>
+          </div>
+
+        </div>
+        <h3 class="animate-charcter" style={{ marginTop: '10%', marginLeft: '10%' }}>
+          Charley Knox</h3>
+
+        <div> <h1 style={{ marginLeft: '8%', marginTop: '-1%' }}>
+          <span>P</span>
+          <span>H</span>
+          <span>O</span>
+          <span>T</span>
+          <span>O</span>
+          <span>G</span>
+          <span>R</span>
+          <span>A</span>
+          <span>P</span>
+          <span>H</span>
+          <span>Y</span>
+
+        </h1></div>
+      </div>
+      <div >
+        <div className='w ' id='f'>
+
+          <div style={{ marginTop: '-2%' }}>
+            <h1 style={{ color: 'white', textAlign: 'center', paddingTop: '5%' }}>
+              Recent Works</h1>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'space-around', padding: '1%' }}>
+            <img className="zoom"
+              src="https://static.wixstatic.com/media/e4f166_88bd4fe30c344d14bed0339e1eb79870~mv2.jpg/v1/fit/w_780,h_520,q_90/e4f166_88bd4fe30c344d14bed0339e1eb79870~mv2.jpg" width="500px" height="500px" />
+            <img className="zoom imgs" src="https://static.wixstatic.com/media/e4f166_dd4834909c62470ab0ce5390a7c9d8d0~mv2.jpg/v1/fill/w_522,h_696,q_90/e4f166_dd4834909c62470ab0ce5390a7c9d8d0~mv2.jpg" width="500px" height="500px">
+            </img>
+            <img className="zoom" src="https://static.wixstatic.com/media/e4f166_aa358ceedb0a48d8a1adf30fd78a0c5f~mv2.jpg/v1/fill/w_522,h_696,q_90/e4f166_aa358ceedb0a48d8a1adf30fd78a0c5f~mv2.jpg" width="500px" height="500px"></img>
+          </div>
+        </div>
+      </div>
+      <div id='ac' >
+        <h1 style={{ color: 'white', textAlign: 'center', backgroundColor: 'black' }}>
+          About</h1>0
+        <div className='t' >
+          <div style={{ display: 'flex', justifyContent: 'space-evenly', padding: '10%' }}>
+            <div>
+              <h1 class="animate-charcter" style={{ marginTop: '10%', marginLeft: '10%' }}>
+                Biography
+              </h1>
+
+              <p style={{ color: 'white', width: '300px' }}>
+                I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+              <p style={{ color: 'white', width: '300px' }}>This is a great space to write a long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide. Tell your visitors the story of how you came up with the idea for your business and what makes you different from your competitors. Make your company stand out and show your visitors who you are.</p>
+            </div>
+            <div className='imgs'>
+              <img src="https://static.wixstatic.com/media/e4f166_7e1a3e75d1c3457ba9cf9ae94106b730~mv2.jpg/v1/fill/w_291,h_435,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/farol-106-c2Zrm27IEyk-unsplash%20(2).jpg" ></img>
+            </div>
+          </div>
+          <div className='gb'>
+            <h1 >
+              Selected clients</h1>
+            <p >I'm a paragraph. Click here to add your own text and edit me. Let your users get to know you.</p>
+          </div>  </div>
+      </div>
+      <div id='aw'>
+        <div style={{ paddingTop: '8%', textAlign: 'center' }}>
+          <h2 className='qw'>
+            Albums</h2>
+        </div>
+
+        <h1 className='er'>Albums</h1>
+      </div>
+      <div style={{ marginTop: '10%', display: 'flex', justifyContent: 'space-around' }}> <div className='kj'>
+        <img src="https://static.wixstatic.com/media/8bb438_22ad7948c19846b1accecec513384bb0~mv2.jpg/v1/fill/w_558,h_372,fp_0.50_0.50,q_90,usm_1.00_0.66_0.01/8bb438_22ad7948c19846b1accecec513384bb0~mv2.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1cm46YXBwOjZiZTRmNGFmMjAzOTQwOTVhZDY5Y2NjMzk4ODkyYzhkIiwib2JqIjpbW3sicGF0aCI6Ii9tZWRpYS84YmI0MzhfMjJhZDc5NDhjMTk4NDZiMWFjY2VjZWM1MTMzODRiYjB-bXYyLmpwZyJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sImlzcyI6InVybjphcHA6NmJlNGY0YWYyMDM5NDA5NWFkNjljY2MzOTg4OTJjOGQiLCJpYXQiOjEwMDAsImp0aSI6IjE3MTk3OTIwMDAiLCJleHAiOjE3MTk3OTIwMDAsIndtayI6eyJwYXRoIjoiL21lZGlhLzhiYjQzOF8zOWE3OGI0NmQ0ZmU0NzA2OWRhNjNkYTkzNDhiNGVlNX5tdjIucG5nIiwib3BhY2l0eSI6MSwicHJvcG9ydGlvbnMiOjAuMSwiZ3Jhdml0eSI6Im5vcnRoLXdlc3QifX0.O5BL78JfIiXKE48DklixZmYLjwmMqTuBNN07TxnuDew" width='400px' height='400px' /></div>
+        <div className='kj'>
+          <img src="https://static.wixstatic.com/media/8bb438_607e8fb1e7e242169d489092dfa49bac~mv2.jpg/v1/fill/w_558,h_457,fp_0.50_0.50,q_90,usm_1.00_0.66_0.01/8bb438_607e8fb1e7e242169d489092dfa49bac~mv2.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1cm46YXBwOjZiZTRmNGFmMjAzOTQwOTVhZDY5Y2NjMzk4ODkyYzhkIiwib2JqIjpbW3sicGF0aCI6Ii9tZWRpYS84YmI0MzhfNjA3ZThmYjFlN2UyNDIxNjlkNDg5MDkyZGZhNDliYWN-bXYyLmpwZyJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sImlzcyI6InVybjphcHA6NmJlNGY0YWYyMDM5NDA5NWFkNjljY2MzOTg4OTJjOGQiLCJpYXQiOjEwMDAsImp0aSI6IjE3MTk3OTIwMDAiLCJleHAiOjE3MTk3OTIwMDAsIndtayI6eyJwYXRoIjoiL21lZGlhLzhiYjQzOF8zOWE3OGI0NmQ0ZmU0NzA2OWRhNjNkYTkzNDhiNGVlNX5tdjIucG5nIiwib3BhY2l0eSI6MSwicHJvcG9ydGlvbnMiOjAuMSwiZ3Jhdml0eSI6Im5vcnRoLXdlc3QifX0.1FcKwvs0ktnwdZFSezm9Yb4LC-Bq0pCT8toj60XtqN4" width='400px' height='400px' /></div>
+
+        <div className='kj'>
+          <img src="https://static.wixstatic.com/media/8bb438_2db448c5d9744318a9958d62af6d8170~mv2.jpg/v1/fill/w_557,h_372,fp_0.50_0.50,q_90,usm_1.00_0.66_0.01/8bb438_2db448c5d9744318a9958d62af6d8170~mv2.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1cm46YXBwOjZiZTRmNGFmMjAzOTQwOTVhZDY5Y2NjMzk4ODkyYzhkIiwib2JqIjpbW3sicGF0aCI6Ii9tZWRpYS84YmI0MzhfMmRiNDQ4YzVkOTc0NDMxOGE5OTU4ZDYyYWY2ZDgxNzB-bXYyLmpwZyJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sImlzcyI6InVybjphcHA6NmJlNGY0YWYyMDM5NDA5NWFkNjljY2MzOTg4OTJjOGQiLCJpYXQiOjEwMDAsImp0aSI6IjE3MTk3OTIwMDAiLCJleHAiOjE3MTk3OTIwMDAsIndtayI6eyJwYXRoIjoiL21lZGlhLzhiYjQzOF8zOWE3OGI0NmQ0ZmU0NzA2OWRhNjNkYTkzNDhiNGVlNX5tdjIucG5nIiwib3BhY2l0eSI6MSwicHJvcG9ydGlvbnMiOjAuMSwiZ3Jhdml0eSI6Im5vcnRoLXdlc3QifX0.3uLKQlBIYdH_JDpEC4KxspAKWtVFUFoNcPYCA6xMv48" width='400px' height='400px' /></div>
+      </div>
+      <div id='we'>
+        <h1 className='rt'>Contact</h1>
+        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+          <div style={{ marginTop: '5%' }}>
+            <h1>Contact</h1>
+            <p style={{ width: '250px', paddingTop: '4%' }}>Charley Knox Is Represented
+              by United Creatives</p>
+            <p style={{ width: '250px', paddingTop: '2%' }}>Email: info@mysite.com
+
+              Phone: 123-456-7890</p>
+          </div>
+          <div style={{ marginTop: '8%', }}>
+            <div style={{ display: 'flex', gap: '11%', justifyContent: 'space-around' }}>
+              <div><label for="fname"  >First name</label><br></br>
+                <input type='text' name='fname' id='fnmae' required className='op'></input></div>
+              <div> <label for="lname">Last name</label><br></br>
+                <input type='text' name='Last name' id='Last name' required className='op'></input></div>
+            </div>
+            <div style={{ marginTop: '8%' }}>
+              <div style={{ display: 'flex', gap: '11%', justifyContent: 'space-around' }}>
+                <div>
+                  <label for="Email"  >Email*</label><br></br>
+                  <input type='email' name='Email' id='Email' required className='op'></input>
+                </div>
+                <div>
+                  <label for="Phone" >Phone </label><br></br>
+                  <input type='tel' name='Last name' id='Last name' required className='op' style={{ marginTop: '1%', width: '200px' }} ></input>
+                </div>
+              </div>
+            </div>
+            <div style={{ marginTop: '10%' }}>
+              <label for="Leave us a message..."   >Leave us a message...*</label><br></br>
+              <input type='text' name='lum' id='lum' required className='ip' style={{ marginTop: '1%' }}></input>
+            </div>
+            <div className='iu'>
+              <button type='button' className='ty'>Submit</button>
+            </div>
+          </div></div>
+
+      </div>
+
+    </div>
+
+
+
+  );
+}
+
+export default App;
